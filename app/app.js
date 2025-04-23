bf4e10ff99f37df8f0c8a1bae51efee1111ddade
LUIZ FELIPE VEIGA SOUZA <felipedesigncont@gmail.com>
2025-04-22 23:26:24 -0300

    finally

--- app/app.js
+++ app/app.js
@@ -1,21 +1,39 @@
 let btn = window.document.querySelector('#btnConvert')
+let valueConversor = window.document.querySelector("#inTemp")
 
-btn.addEventListener('click', ()=>{
-    let optConversor = window.document.querySelector("#tempIn").value
-    let optConvertido = window.document.querySelector("#tempOut").value
-    let valueConversor = window.document.querySelector("#inTemp").value
-    let saida = window.document.querySelector("#output")
-
-    if (optConversor == optConvertido) {
-        saida.innerHTML = valueConversor
-    } else {
-        saida.innerHTML = converter(optConversor, optConvertido, valueConversor)
+valueConversor.addEventListener('keypress', (e)=>{
+    if (e.charCode ==13){
+        convertion()
     }
-    
 })
 
+function convertion(){
+    let optConversor = window.document.querySelector("#tempIn").value
+    let optConvertido = window.document.querySelector("#tempOut").value
+    let saida = window.document.querySelector("#output")
+
+    let check = checked(optConversor, valueConversor, saida)
+    saida.innerHTML = converter(optConversor, optConvertido, valueConversor.value).toFixed(2)
+}
+
+btn.addEventListener('click', convertion)
+
+function checked(opcaoI, valorI, output){
+    if ((opcaoI == 'kelvin') && (valorI<0)){
+        alert('Kelvin não pode ser negativo')
+        output.innerHTML = 'Valor inválido'
+        return false
+    }
+    if (valorI==''){
+        alert('Preencha um valor')
+        return false
+    }
+    return true
+}
 function converter(opcaoI, opcaoO, valorI){
-    if ((opcaoI == "fahrenheit") && (opcaoO == "celsius")){
+    if (opcaoI == opcaoO) {
+        return Number(valorI)
+    } else if ((opcaoI == "fahrenheit") && (opcaoO == "celsius")){
         return (valorI-32)/1.8
     } else if ((opcaoI == "fahrenheit") && (opcaoO == "kelvin")){
         return (valorI-32)*5/9+273
